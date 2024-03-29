f955662261c43306fc5da0200799f994c338df45
yousseflearn <youssefeasylearn@gmail.com>
2024-03-29 11:16:45 -0700

    Create Register Page

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,9 +1,81 @@
 import "./Register.scss";
+import "../../App.scss";
+import { Link } from "react-router-dom";
+// import our assets
+import video from "../../loginAsset/video5.mp4";
+import logo from "../../loginAsset/hero12.png";
+// import icons
+import { FaUserShield } from "react-icons/fa";
+import { BsFillShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
+import { MdMarkEmailRead } from "react-icons/md";
 
 const Register = () => {
   return (
-    <div>
-      This is Register Page <a href="/">To Login</a>
+    <div className="registerPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+
+          <div className="textDiv">
+            <h2 className="title">Create and Boost Your Project</h2>
+            <p>Adopt Your Website With Your Project</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">have An Account</span>
+            <Link to={"/"}>
+              <button className="btn">Login</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>Let Us Know You!</h3>
+          </div>
+
+          <form action="" className="form grid">
+            <div className="inputDiv">
+              <label htmlFor="email">Email</label>
+              <div className="input flex">
+                <MdMarkEmailRead className="icon" />
+                <input type="email" id="email" placeholder="Enter Your Email" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="username">Username</label>
+              <div className="input flex">
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsFillShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Register </span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot Your Password? <a href="">Click Here</a>
+            </span>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
